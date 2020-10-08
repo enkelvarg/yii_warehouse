d3c6b94fa822d4bf0f9a7a31bee6f00d3d385f0f
enkelvarg <warrrgh666@gmail.com>
2020-10-08 13:47:25 +0300

    access control on device and store pages

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -30,7 +30,7 @@ class SiteController extends Controller
                 'class' => AccessControl::className(),
                 'rules' => [
                     [
-                        'actions' => ['signup', 'login'],
+                        'actions' => ['index', 'signup', 'login'],
                         'allow' => true,
                         'roles' => ['?'],
                     ],
